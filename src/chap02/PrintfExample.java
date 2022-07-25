a4d04c40b7b52b4a443d3f7e99222c23ddd590fd
sanhopmj <sanho.pmj@gmail.com>
2022-07-26 01:36:23 +0900

    printf() 메소드 사용 방법

--- src/chap02/PrintfExample.java
+++ src/chap02/PrintfExample.java
@@ -2,7 +2,7 @@ package chap02;
 
 public class PrintfExample {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {  
 		int value = 123; 
 		System.out.printf("상품의 가격 : %d원\n", value);
 		System.out.printf("상품의 가격 : %6d원\n", value);
