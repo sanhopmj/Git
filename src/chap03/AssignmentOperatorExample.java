ed67a7a796d213cf77ed254e5d2c8028fdf17903
sanhopmj <sanho.pmj@gmail.com>
2022-07-26 02:15:24 +0900

    대입 연산자

---
+++ src/chap03/AssignmentOperatorExample.java
@@ -0,0 +1,25 @@
+package chap03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		int result = 0;
+		result += 10; // result = 10
+		System.out.println("result= "+ result);
+		
+		result -= 5; // result = 5
+		System.out.println("result= "+ result);
+		
+		result *= 3; //result = 15
+		System.out.println("result= "+ result);
+		
+		result /= 5; //result = 3
+		System.out.println("result= "+ result);
+		
+		result %= 3; //result = 0
+		System.out.println("result= "+ result);
+		
+
+	}
+
+}
