dd7a4cb17e4688a4480c23a12bc2a1a9d2162db7
sanhopmj <sanho.pmj@gmail.com>
2022-07-26 17:25:57 +0900

    1부터 10까지 출력

---
+++ src/chap04/ForPrintFromTo10Example.java
@@ -0,0 +1,13 @@
+package chap04;
+
+public class ForPrintFromTo10Example {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <=10 ; i ++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
