854322c4b7bf35629b201000853651c06e73e06b
sanhopmj <sanho.pmj@gmail.com>
2022-07-27 01:01:58 +0900

    연습문제 4-6

---
+++ src/chap04/p46.java
@@ -0,0 +1,20 @@
+package chap04;
+
+public class p46 {
+
+	public static void main(String[] args) {
+		for(int i =1 ; i < 5; i ++) {
+			for (int j = 4; j >0 ; j-- ) {
+				if(i<j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
